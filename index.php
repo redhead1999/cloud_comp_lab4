a12af43ee77b436a7a10ad30873b223d6dbb5ebd
Yury Kirilin <t70052@gmail.com>
2021-04-02 15:18:24 +0400

    v1

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>PHP Starter Application</title>
+	<meta http-equiv="Content-Type"
+         content="text/html; charset=utf-8">
+	<link rel="stylesheet" href="style.css" />
+</head>
+<body>
+<?php
+  phpinfo();
+?>
+</body>
+</html>
